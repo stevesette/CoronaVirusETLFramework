4746e15f0baa73c870ec3abd2fd92d2c5dd8fa40
Tim Lexa <lexa.t@husky.neu.edu>
2020-03-29 14:09:45 -0400

    Expanded yaml loader, edited the topN yaml file (just gonna delete and use Jack's new one). Added the CSV

--- reading_engine.py
+++ reading_engine.py
@@ -9,19 +9,18 @@ def read_config(filepath):
     if not filepath.endswith(".yaml"):
         raise RuntimeError("Not a yaml file")
     with open(filepath) as f:
-        yaml.load(f)
+        data = yaml.load(f, Loader=yaml.FullLoader)
+        print(data)
 
+# def handle_yaml_parameters(parameters):
+#     pass
 
-def handle_yaml_parameters(parameters):
-    pass
+# def output(output_object):
+#     pass
 
+# def main_driver(filepath):
+    # file = read_config(filepath)
+    # yaml_ran = handle_yaml_parameters(file)
+    # output(file)
 
-def output(output_object):
-    pass
-
-
-def main_driver(filepath):
-    file = read_config(filepath)
-    yaml_ran = handle_yaml_parameters(file)
-    output(yaml_ran)
-
+print(read_config('/Users/Tim/Desktop/Final 4300 Project/CoronaVirusETLFramework/topN.yaml'))
