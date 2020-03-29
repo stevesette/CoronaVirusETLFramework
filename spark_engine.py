009e9fa340d607b80510af0354069788c87a5b8c
stevesette <setteducati.s@husky.neu.edu>
2020-03-29 13:48:25 -0400

    spark starter

---
+++ spark_engine.py
@@ -0,0 +1,28 @@
+import warnings
+
+import findspark
+import pyspark as ps
+from pyspark.sql import SQLContext
+
+
+class SparkEngine:
+    def __init__(self):
+        findspark.init()
+        try:
+            # create SparkContext on all CPUs available: in my case I have 4 CPUs on my laptop
+            self.sc = ps.SparkContext('local[4]')
+            self.sqlContext = SQLContext(self.sc)
+            print("Just created a SparkContext")
+        except ValueError:
+            warnings.warn("SparkContext already exists in this scope")
+        self.data = {}
+
+    def read_csv(self, filepath, df_name):
+        self.data[df_name] = self.sqlContext.read.format('com.databricks.spark.csv').options(header='false').load(filepath)
+        return
+
+    def join_data(self, name1, name2, new_name, list_of_col_pairs):
+        data1 = self.data[name1]
+        data2 = self.data[name2]
+        self.data[new_name] = data1.join(data2, list_of_col_pairs)
+        return
