d15caa3aa9cffc5fc69e6e206098c74c63a31309
Tim Lexa <lexa.t@husky.neu.edu>
2020-03-30 20:37:42 -0400

    Added functions to verify dates. Both to make sure dates are valid, and that end date comes after start date

--- reading_engine.py
+++ reading_engine.py
@@ -1,7 +1,5 @@
-import os
-
 import yaml
-
+import datetime
 
 def read_config(filepath):
     # if os.getcwd()
@@ -10,7 +8,37 @@ def read_config(filepath):
         raise RuntimeError("Not a yaml file")
     with open(filepath) as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
-        print(data)
+    return data
+
+fields = []
+
+def window_checker(filepath):
+    data = read_config(filepath)
+    if 'window' not in data.keys():
+        return True
+    if 'window' in data.keys():
+        if date_checker(data['window']):
+            if timing_checker(data['window']):
+                return True
+            else:
+                print('Your end_date comes before your start_date. Correct the issue in order to proceed.')
+                return False
+        else:
+            print('One or both of your dates is invalid. Reformat to follow: mm/dd/yyyy')
+            return False
+
+def date_checker(date_data):
+    dates = [date_data['start_date'], date_data['end_date']]
+    for date in dates:
+        month, day, year = date.split('/')
+        try:
+            datetime.datetime(int(year), int(month), int(day))
+        except ValueError:
+            return False
+    return True
+
+def timing_checker(date_data):
+    return date_data['start_date'] <= date_data['end_date']
 
 # def handle_yaml_parameters(parameters):
 #     pass
@@ -22,5 +50,5 @@ def read_config(filepath):
     # file = read_config(filepath)
     # yaml_ran = handle_yaml_parameters(file)
     # output(file)
-
-print(read_config('/Users/Tim/Desktop/Final 4300 Project/CoronaVirusETLFramework/topN.yaml'))
+#
+print(window_checker('/Users/Tim/Desktop/Final 4300 Project/CoronaVirusETLFramework/mostNew.yaml'))
