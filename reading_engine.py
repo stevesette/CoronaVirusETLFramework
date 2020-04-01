d2f192915cdcf586c028154c9fafcc77e9ab6b06
Tim Lexa <lexa.t@husky.neu.edu>
2020-04-01 02:15:59 -0400

    Added a bunch of checker functions, along with a handler to call all of them at once. Think we're done with checkers now

--- reading_engine.py
+++ reading_engine.py
@@ -1,6 +1,7 @@
 import yaml
 import datetime
 
+
 def read_config(filepath):
     # if os.getcwd()
     # do some sort of error handle and or checking if a file exists
@@ -10,26 +11,31 @@ def read_config(filepath):
         data = yaml.load(f, Loader=yaml.FullLoader)
     return data
 
-fields = []
 
-def window_checker(filepath):
-    data = read_config(filepath) #Once main handler is created, this will take a dictionary instead of path
-    if 'window' not in data.keys():
-        return True
-    if 'window' in data.keys():
-        if 'start_date' in data['window'].keys() and 'end_date' in data['window'].keys():
-            if date_checker(data['window']):
-                if timing_checker(data['window']):
-                    return True
-                else:
-                    print('Your end_date comes before your start_date. Correct the issue in order to proceed.')
-                    return False
-            else:
-                print('One or both of your dates is invalid. Reformat to follow: mm/dd/yyyy')
-                return False
-        else:
-            print('Window is missing \'start_date\' and/or \'end_date\'')
+# This will contain all possible field names from our datasets, once they're finished
+fields = ['cases', 'deaths', 'recoveries', 'mortality_rate', 'recovery_rate']
+
+
+def field_checker(field_list, d):
+    fields_in_dict = [value for value in d['aggregate']['fields']]
+    for f in fields_in_dict:
+        if f not in field_list:
             return False
+    if 'rank_field' in d.keys():
+        if d['rank_field'] not in field_list:
+            return False
+    return True
+
+
+def top_n_checker(d):
+    if 'top_n' not in d.keys():
+        return True
+    try:
+        int(d['top_n'])
+        return True
+    except ValueError:
+        return False
+
 
 def date_checker(date_data):
     dates = [date_data['start_date'], date_data['end_date']]
@@ -41,8 +47,57 @@ def date_checker(date_data):
             return False
     return True
 
+
 def timing_checker(date_data):
     return date_data['start_date'] <= date_data['end_date']
+
+
+def window_checker(d):
+    if 'window' not in d.keys():
+        return True
+    if 'window' in d.keys():
+        if 'start_date' in d['window'].keys() and 'end_date' in d['window'].keys():
+            if date_checker(d['window']):
+                if timing_checker(d['window']):
+                    return True
+                else:
+                    print('Your end_date comes before your start_date. Correct the issue in order to proceed.')
+                    return False
+            else:
+                print('One or both of your dates is invalid. Reformat to follow: mm/dd/yyyy')
+                return False
+        else:
+            print('Window is missing \'start_date\' and/or \'end_date\'')
+            return False
+
+
+def most_least_checker(d):
+    if 'most_least' not in d.keys():
+        return True
+    if d['most_least'] == 'most' or d['most_least'] == 'least':
+        return True
+    return False
+
+
+def input_output_checker(d):
+    if d['input_file'] == 'data.csv' and d['output_method'] == 'console':
+        return True
+
+
+def yaml_handler(filepath):
+    data = read_config(filepath)
+    if (
+            window_checker(data) and
+            field_checker(fields, data) and
+            top_n_checker(data) and
+            most_least_checker(data) and
+            input_output_checker(data)
+            # Need to have something to check compare? Not sure on this since we're comparing states vs. counties, etc.
+    ):
+        return True
+
+
+print(yaml_handler('/Users/Tim/Desktop/Final 4300 Project/CoronaVirusETLFramework/config_files/topN.yaml'))
 
 # def handle_yaml_parameters(parameters):
 #     pass
@@ -51,8 +106,7 @@ def timing_checker(date_data):
 #     pass
 
 # def main_driver(filepath):
-    # file = read_config(filepath)
-    # yaml_ran = handle_yaml_parameters(file)
-    # output(file)
+#     file = read_config(filepath)
+#     yaml_ran = handle_yaml_parameters(file)
+#     output(file)
 #
-print(window_checker('/Users/Tim/Desktop/Final 4300 Project/CoronaVirusETLFramework/mostNew.yaml'))
