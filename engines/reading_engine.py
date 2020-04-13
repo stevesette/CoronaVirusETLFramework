539b1e9e452367d776ee79a54d28475d6e59f8c4
Steven Setteducati Jr <stevesetteducatijr@gmail.com>
2020-04-12 21:33:12 -0400

    Reader engine object (#15)
    * Starting to change to an object
    
    * Script is not an object, most functions work. Needs to be tweaked a bit, but should be run-able
    
    Co-authored-by: Tim Lexa <lexa.t@husky.neu.edu>

--- engines/reading_engine.py
+++ engines/reading_engine.py
@@ -1,139 +1,149 @@
 import yaml
 import datetime
 
+class ReadingEngine:
+    def __init__(self, yaml_file):
+        self.file_name = yaml_file
+        self.fields = {
+            'counties':
+                ['county', 'new_cases', 'new_deaths', 'cases', 'deaths',
+                 'mortality_rate', 'mortality_rate_delta', 'new_cases_delta',
+                 'new_deaths_delta', 'recoveries', 'recovery_rate'],
+            'tests':
+                ['positive', 'positive_tests', 'negative', 'totalResults',
+                 'total_tests', 'negative_tests', 'test_positive_rate', 'test_negative_rate']
+        }
+        self.categories = ['min', 'max', 'mean', 'sum', 'fields']
+        self.data = self.yaml_handler()
 
-def read_config(filepath):
-    # if os.getcwd()
-    # do some sort of error handle and or checking if a file exists
-    if not filepath.endswith(".yaml"):
-        raise RuntimeError("Not a yaml file")
-    with open(filepath) as f:
-        data = yaml.load(f, Loader=yaml.FullLoader)
-    return data
+    def read_config(self, filepath):
+        if not filepath.endswith(".yaml"):
+            raise RuntimeError("Not a yaml file")
+        with open(filepath) as f:
+            data = yaml.load(f, Loader=yaml.FullLoader)
+        return data
 
-
-fields = {
-    'counties':
-        ['county', 'new_cases', 'new_deaths', 'cases', 'deaths',
-         'mortality_rate', 'mortality_rate_delta', 'new_cases_delta',
-         'new_deaths_delta', 'recoveries', 'recovery_rate'],
-    'tests':
-        ['positive', 'positive_tests', 'negative', 'totalResults',
-         'total_tests', 'negative_tests', 'test_positive_rate', 'test_negative_rate']
-}
-categories = ['min', 'max', 'mean', 'sum', 'fields']
-
-
-def field_checker(field_dict, d):
-    for category in categories:
-        if category in d['aggregate'].keys():
-            for field in d['aggregate'][category]:
-                if field not in field_dict['counties'] and field not in field_dict['tests']:
-                    return False
-    return True
-
-
-def file_picker(field_dict, d):
-    needed_files = []
-    counties = 'CoronaVirusETLFramework/data/us-counties.csv'
-    tests = 'CoronaVirusETLFramework/data/tests-by-state.csv'
-    for category in categories:
-        if category in d['aggregate'].keys():
-            for field in d['aggregate'][category]:
-                if field in field_dict['counties'] and counties not in needed_files:
-                    needed_files.append(counties)
-                if field in field_dict['tests'] and tests not in needed_files:
-                    needed_files.append(tests)
-    return needed_files
-
-
-def top_n_checker(d):
-    if 'top_n' not in d.keys():
+    def field_checker(self, d):
+        for category in self.categories:
+            if category in d['aggregate'].keys():
+                for field in d['aggregate'][category]:
+                    if field not in self.fields['counties'] and field not in self.fields['tests']:
+                        return False
         return True
-    try:
-        int(d['top_n'])
-        return True
-    except ValueError:
-        return False
 
+    def group_by(self):
+        groups = []
+        d = self.data['aggregate']
+        if 'area' in d.keys():
+            groups.append(self.level)
+        if 'window' in d.keys():
+            groups.append(self.window_getter())
+        return groups
 
-def area_checker(d):
-    agg = d['aggregate']
-    accepted = ['state', 'region', 'county']
-    if 'area' not in agg.keys():
-        return True
-    if 'area' in agg.keys():
-        if agg['area'] in accepted:
+    def function_finder(self):
+        d = self.data['aggregate'].copy()
+        for key in d.keys():
+            if key not in self.categories:
+                d.pop(key)
+        return d
+
+    def file_picker(self):
+        d = self.data
+        needed_files = []
+        self.needed_columns = set()
+        counties = 'CoronaVirusETLFramework/data/us-counties.csv'
+        tests = 'CoronaVirusETLFramework/data/tests-by-state.csv'
+        for category in self.categories:
+            if category in d['aggregate'].keys():
+                for field in d['aggregate'][category]:
+                    self.needed_columns.add(field)
+                    if field in self.fields['counties'] and counties not in needed_files:
+                        needed_files.append(counties)
+                    if field in self.fields['tests'] and tests not in needed_files:
+                        needed_files.append(tests)
+        return needed_files
+
+    def get_columns(self):
+        return self.needed_columns
+
+    def area_checker(self, d):
+        agg = d['aggregate']
+        accepted = ['state', 'region', 'county']
+        if 'area' not in agg.keys():
             return True
+        if 'area' in agg.keys():
+            if agg['area'] in accepted:
+                self.level = agg['area']
+                return True
+            else:
+                print(agg['area'] + ' is not accepted as an area.')
 
+    def get_level(self):
+        return self.level
 
-def date_checker(date_data):
-    dates = [date_data['start_date'], date_data['end_date']]
-    for date in dates:
-        month, day, year = date.split('/')
-        try:
-            datetime.datetime(int(year), int(month), int(day))
-        except ValueError:
-            return False
-    return True
-
-
-def timing_checker(date_data):
-    return date_data['start_date'] <= date_data['end_date']
-
-
-def window_checker(d):
-    agg = d['aggregate']
-    if 'window' not in agg.keys():
+    def date_checker(self, date_data):
+        dates = [date_data['start_date'], date_data['end_date']]
+        for date in dates:
+            month, day, year = date.split('/')
+            try:
+                datetime.datetime(int(year), int(month), int(day))
+            except ValueError:
+                print('The dates you entered are invalid')
+                return False
         return True
-    if 'window' in agg.keys():
-        if 'start_date' in agg['window'].keys() and 'end_date' in agg['window'].keys():
-            if date_checker(agg['window']):
-                if timing_checker(agg['window']):
-                    return True
+
+    def timing_checker(self, date_data):
+        return date_data['start_date'] <= date_data['end_date']
+
+    def window_checker(self, d):
+        agg = d['aggregate']
+        if 'window' not in agg.keys():
+            return True
+        if 'window' in agg.keys():
+            if 'start_date' in agg['window'].keys() and 'end_date' in agg['window'].keys():
+                if self.date_checker(agg['window']):
+                    if self.timing_checker(agg['window']):
+                        return True
+                    else:
+                        print('Your end_date comes before your start_date. Correct the issue in order to proceed.')
+                        return False
                 else:
-                    print('Your end_date comes before your start_date. Correct the issue in order to proceed.')
+                    print('One or both of your dates is invalid. Reformat to follow: mm/dd/yyyy')
                     return False
             else:
-                print('One or both of your dates is invalid. Reformat to follow: mm/dd/yyyy')
+                print('Window is missing \'start_date\' and/or \'end_date\'')
                 return False
+
+    def output_checker(self, d):
+        if d['output_method'] == 'console':
+            return True
         else:
-            print('Window is missing \'start_date\' and/or \'end_date\'')
             return False
 
+    def window_creator(self, d):
+        data = d['aggregate']
+        if 'window' not in data:
+            window = None
+        else:
+            window = (data['window']['start_date'], data['window']['end_date'])
+        return window
 
-def input_output_checker(d):
-    if d['output_method'] == 'console':
-        return True
-    else:
-        return False
+    def window_getter(self):
+        return self.window_creator(self.data)
 
-
-def window_creator(d):
-    data = d['aggregate']
-    if 'window' not in data:
-        window = None
-    else:
-        window = (data['window']['start_date'], data['window']['end_date'])
-    return window
-
-
-def yaml_handler(filepath):
-    data = read_config(filepath)
-    if \
-            (
-                    window_checker(data) and
-                    field_checker(fields, data) and
-                    top_n_checker(data) and
-                    input_output_checker(data) and
-                    area_checker(data)
-            ):
-        return True
-    else:
-        return False
-
-
-print(yaml_handler('/Users/Tim/Desktop/Final-4300-Project/CoronaVirusETLFramework/config_files/compare.yaml'))
+    def yaml_handler(self):
+        filepath = 'config_files/' + self.file_name
+        data = self.read_config(filepath)
+        if \
+                (
+                        self.window_checker(data) and
+                        self.field_checker(self.fields, data) and
+                        self.output_checker(data) and
+                        self.area_checker(data)
+                ):
+            return data
+        else:
+            raise RuntimeError('Error reading config file')
 
 # def handle_yaml_parameters(parameters):
 #     pass
