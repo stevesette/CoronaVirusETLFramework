e496c48d4b0ad93eec2e4d6dc15abe9d730a30ff
Jack Tonina <tonina.j@husky.neu.edu>
2020-04-12 20:23:00 -0400

    updated output johnson

---
+++ engines/output_engine.py
@@ -0,0 +1,25 @@
+class OutputEngine:
+    def __init__(self, df, output_type):
+        self.df = df
+        self.output_type = output_type
+
+    def output(self):
+        if self.output_type == 'terminal':
+            self.output_df()
+        elif '.csv' in self.output_type:
+            self.output_csv()
+        elif '.txt' in self.output_type:
+            self.output_txt()
+        else:
+            raise RuntimeError("invalid output option")
+
+    def output_df(self):
+        return self.df
+
+    def output_csv(self):
+        self.df.write.format('csv').save(self.output_type)
+        # 'com.databricks.spark.csv' if no run, try instead of 'csv'
+
+    def output_textfile(self):
+        self.df.write.format('csv').options("delimiter", "|").save(self.output_type)
+        # 'com.databricks.spark.csv' if no run, try instead of 'csv'
